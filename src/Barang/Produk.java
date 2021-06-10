9992a98dd1fbdfc7c095448fb55a66d0c5090287
HazielC <haziel_justice_29rpl@student.smktelkom-mlg.sch.id>
2021-06-10 09:02:38 +0700

    hore berhasil

---
+++ src/Barang/Produk.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Barang;
+
+
+public class Produk {
+    
+public static void produk1 (){
+System.out.println("Rinso");
+}
+public static void produk2 (){
+System.out.println("Baygon");
+}
+public static void produk3 (){
+System.out.println("Molto");
+}
+public static void produk4 (){
+System.out.println("Indomie");
+}
+public static void harga1 (){
+System.out.println("Rp. 5.000");
+}
+public static void harga2 (){
+System.out.println("Rp. 17.000");
+}
+public static void harga3 (){
+System.out.println("Rp. 10.500");
+}
+public static void harga4 (){
+System.out.println("Rp. 2.500");
+}
+
+public static void main(String[] args) {
+    
+System.out.println ("===== DATA HARGA BARANG =====");
+System.out.print ("Produk 1 : ");
+produk1();
+System.out.print ("Harga : ");
+harga1();
+System.out.print ("Produk 2 : ");
+produk2();
+System.out.print ("Harga : ");
+harga2();
+System.out.print ("Produk 3 : ");
+produk3();
+System.out.print ("Harga : ");
+harga3();
+System.out.print ("Produk 4 : ");
+produk4();
+System.out.print ("Harga : ");
+harga4();
+
+    }
+}
+    
+
